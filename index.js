1bd504a4c51ea664d1ae48884314e7ca903d119b
adamscheer4 <adam.scheer28@student.tamdistrict.org>
2025-05-20 21:00:39 +0000

    Add Inital Files

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const blackjackButtonEl = document.getElementById("blackjackButton");
+blackjackButtonEl.addEventListener("click", blackjack)
+
+function blackjack(){
+    console.log("bruh")
+}
+
+const pokerButtonEl = document.getElementById("pokerButton");
+pokerButtonEl.addEventListener("click", poker)
+
+function poker(){
+    console.log("what the hellionte")
+}
+
+const rouletteButtonEl = document.getElementById("rouletteButton");
+rouletteButtonEl.addEventListener("click", roulette)
+
+function roulette(){
+    console.log("Diddy")
+}
